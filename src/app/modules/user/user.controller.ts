78ae0e9d03dbc8629c113e573ebb14fa13d4a527
unknown <samiraborsha08@gmail.com>
2025-08-31 14:43:27 +0600

    feat: implemente catchAsync utility functions to handle try catch

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,38 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Response, Request } from "express";
 import { UserService } from "./user.service";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatusCode from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const user = await UserService.createUser(req.body);
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const user = await UserService.createUser(req.body);
 
-    sendResponse(res, {
-      statusCode: httpStatusCode.CREATED,
-      success: true,
-      message: "User created successfully",
-      data: user,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatusCode.CREATED,
+    success: true,
+    message: "User created successfully",
+    data: user,
+  });
+});
 
-const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await UserService.getAllUsers();
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const result = await UserService.getAllUsers();
 
-    sendResponse(res, {
-      statusCode: httpStatusCode.OK,
-      success: true,
-      message: "All users retrieved successfully",
-      data: result.data,
-      meta: result.meta,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatusCode.OK,
+    success: true,
+    message: "All users retrieved successfully",
+    data: result.data,
+    meta: result.meta,
+  });
+});
 
 export const UserController = {
   createUser,
